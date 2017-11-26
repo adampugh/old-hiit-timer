895b9ee9d2826afe00884e211921cd416421b9eb
Adam Pugh <adampugh@zoho.com>
2017-11-26 15:53:36 +0000

    Updated navbar

--- src/views/navbar-loggedin.js
+++ src/views/navbar-loggedin.js
@@ -9,23 +9,25 @@ class NavbarLoggedIn extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div className="container-fluid">
                 <nav className="row">
-                    <div className="navigation-bar">
-                        <div className="navigation-bar__logo">
-                            <h1 className="logo">HIIT Timer</h1>
-                        </div>
-                        <div className="navigation-bar__links">
-                            <label className="hamburger-icon" onClick={this.handleHamburgerClick}>&#9776;</label>
-                            <div className="menu">
-                                <a href="#">Workouts</a>
-                                <a href="#">Create</a>
-                                <a href="#">Logout</a>
+                    <div className="container">
+                        <div className="navigation-bar">
+                            <div className="navigation-bar__logo">
+                                <h1 className="logo">HIIT Timer</h1>
                             </div>
-                            <div className="menu--desktop">
-                                <a href="#">Workouts</a>
-                                <a href="#">Create</a>
-                                <a href="#">Logout</a>
+                            <div className="navigation-bar__links">
+                                <label className="hamburger-icon" onClick={this.handleHamburgerClick}>&#9776;</label>
+                                <div className="menu">
+                                    <a href="#">Workouts</a>
+                                    <a href="#">Create</a>
+                                    <a href="#">Logout</a>
+                                </div>
+                                <div className="menu--desktop">
+                                    <a href="#">Workouts</a>
+                                    <a href="#">Create</a>
+                                    <a href="#">Logout</a>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/views/navbar-loggedout.js
+++ src/views/navbar-loggedout.js
@@ -9,21 +9,23 @@ class NavbarLoggedOut extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div className="container-fluid">
                 <nav className="row">
-                    <div className="navigation-bar">
-                        <div className="navigation-bar__logo">
-                            <h1 className="logo">HIIT Timer</h1>
-                        </div>
-                        <div className="navigation-bar__links">
-                            <label className="hamburger-icon" onClick={this.handleHamburgerClick}>&#9776;</label>
-                            <div className="menu">
-                                <a href="#">Sign Up</a>
-                                <a href="#">Login</a>
+                    <div className="container">
+                        <div className="navigation-bar">
+                            <div className="navigation-bar__logo">
+                                <h1 className="logo">HIIT Timer</h1>
                             </div>
-                            <div className="menu--desktop">
-                                <a href="#">Sign Up</a>
-                                <a href="#">Login</a>
+                            <div className="navigation-bar__links">
+                                <label className="hamburger-icon" onClick={this.handleHamburgerClick}>&#9776;</label>
+                                <div className="menu">
+                                    <a href="#">Sign Up</a>
+                                    <a href="#">Login</a>
+                                </div>
+                                <div className="menu--desktop">
+                                    <a href="#">Sign Up</a>
+                                    <a href="#">Login</a>
+                                </div>
                             </div>
                         </div>
                     </div>
