2a51cf8972b3f25062ba6cabfa0ad57d00de3afb
Adam Pugh <adampugh@zoho.com>
2018-01-18 20:17:14 +0000

    Updated workoutform

--- src/views/createworkoutpage/workoutform.js
+++ src/views/createworkoutpage/workoutform.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React { Component } from "react";
 
 // gut this - use modal instead
-export default class WorkoutForm extends React.Component {
+export default class WorkoutForm extends Component {
     render() {
         return (
             <div>
